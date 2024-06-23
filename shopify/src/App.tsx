a566760961e78726d6a44ab2903cfb3297597966
Abhijith B <babhijith58@gmail.com>
2024-06-23 08:06:07 +0000

    heading style added

--- shopify/src/App.tsx
+++ shopify/src/App.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 
 import './App.css';
 
 function App() {
+  const {isTop,setIsTop}= useState(false);
+
+  useEffect(()=>{
+    function moveTop(){
+      if(window.scrollY>0){
+        setIsTop(false);
+    }
+  }
+});
+
+  
   return (
-    <div className="App">
-    
-    <h1>Hello Shopify</h1>
+    <div className="app">
+    <header className='app-header'>
+    <h2 className= {`title ${isTop ? "top" : ""}`}>Shopify</h2>
+    </header>
     </div>
   );
 }
